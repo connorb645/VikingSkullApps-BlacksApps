828e7df55e5d06a153538088c713c35e383621a7
Connor Black <connor.b645@gmail.com>
2022-02-09 20:30:18 +0000

    Added mock data for cryptocoins

---
+++ VikingSkullApps-BlacksApps/VikingSkullApps-BlacksApps/UI Clones/Coinbase/CoinbaseScreenView.swift
@@ -0,0 +1,20 @@
+//
+//  Coinbase.swift
+//  VikingSkullApps-BlacksApps
+//
+//  Created by Connor Black on 09/02/2022.
+//
+
+import SwiftUI
+
+struct CoinbaseScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Coinbase_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinbaseScreenView()
+    }
+}
---
+++ VikingSkullApps-BlacksApps/VikingSkullApps-BlacksApps/UI Clones/Coinbase/CoinbaseScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  CoinbaseScreenViewModel.swift
+//  VikingSkullApps-BlacksApps
+//
+//  Created by Connor Black on 09/02/2022.
+//
+
+import Foundation
+
+struct CoinbaseScreenViewModel: ObservableObject {
+    
+}
